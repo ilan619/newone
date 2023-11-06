3e60c9fa21ce4074058e304efa3b3acb67ca8f1c
ilan619 <ilankee1811@gmail.com>
2023-11-06 10:47:01 +0530

    new-code

---
+++ CommonTrainingCustomFilter.java
@@ -0,0 +1,9 @@
+package com.training.mappers;
+
+import com.tagit.commons.core.filter.v2.CommonsStrictChannelProcessFilter;
+
+public class CommonTrainingCustomFilter extends CommonsStrictChannelProcessFilter {
+	
+	
+
+}
