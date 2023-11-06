2c41ce1f97fc4cecb691319537c70baaec95c14b
ilan619 <149923914+ilan619@users.noreply.github.com>
2023-11-06 11:01:29 +0530

    Update CommonTrainingCustomFilter.java

--- CommonTrainingCustomFilter.java
+++ CommonTrainingCustomFilter.java
@@ -4,6 +4,6 @@ import com.tagit.commons.core.filter.v2.CommonsStrictChannelProcessFilter;
 
 public class CommonTrainingCustomFilter extends CommonsStrictChannelProcessFilter {
 	System.out.println("good morning da ilan");
-	
+	System.out.println("have a good day");
 
 }
